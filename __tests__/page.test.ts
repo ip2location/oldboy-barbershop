ec9e51d0a2e0866963a305be8ea906c39c493afd
Alinochka07 <alina011988@icloud.com>
2023-07-07 13:19:14 +0600

    feature/OLDBOY-4_added-jest

---
+++ __tests__/page.test.ts
@@ -0,0 +1,16 @@
+import {describe, expect, test} from '@jest/globals';
+// import { render, screen } from '@testing-library/react'
+import Home from "../src/app/page";
+
+describe('Home', () => {
+    test('testing a home page', () => {
+        expect(Home)
+    })
+
+  //   const home = screen.getByRole('heading', {
+  //     name: /welcome to next\.js!/i,
+  //   })
+
+  //   expect(heading).toBeInTheDocument()
+  // })
+})
---
+++ jest.setup.js
@@ -0,0 +1,4 @@
+import '@testing-library/jest-dom/extend-expect';
+
+setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
+
