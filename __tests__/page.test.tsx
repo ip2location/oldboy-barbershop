422bdc74a7df63ee86f7a6158ae113ead07118bf
Alexander Zharkov <aleksandr.zharkov1@epam.com>
2023-07-13 00:42:21 +0600

    Fix conflicts

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,11 +1,11 @@
-import Home from "../src/app/page";
-import { render, screen } from "@testing-library/react";
+import Home from '../src/app/page';
+import { render, screen } from '@testing-library/react';
 
-
-describe("Home", () => {
-    it('should pass all the tests', () => {
-    render(<Home/>)
-    expect(screen.getByText("Hello world!")).toBeInTheDocument();
-    });
+describe('Home', () => {
+  it('should pass all the tests', () => {
+    render(<Home />);
+    expect(
+      screen.getByText('Instantly deploy your Next.js site to a shareable URL with Vercel.'),
+    ).toBeInTheDocument();
+  });
 });
-
