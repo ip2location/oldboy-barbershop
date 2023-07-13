c7a8603ece591ce9bd1de8d772008db9d54859a0
Alinochka07 <alina011988@icloud.com>
2023-07-13 22:47:00 +0600

    feature/header using tailwind

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,44 @@
+import Image from 'next/image';
+
+const mainNavItems = [
+  { title: 'Главная', href: '#' },
+  { title: 'Услуги', href: '#' },
+  { title: 'Косметика', href: '#' },
+  { title: 'Блог', href: '#' },
+  { title: 'Франшиза', href: '#' },
+  { title: 'Инвестиции', href: '#' },
+  { title: 'Вакансии', href: '#' },
+  { title: 'Барберы', href: '#' },
+  { title: 'Контакты', href: '#' },
+  { title: 'Филиалы', href: '#' },
+];
+
+const Header = () => {
+  return (
+    <header className="hero relative pt-12 px-12 pb-17 overflow-hidden bg-header-bg text-white">
+      <div className="hero__top relative flex mb-5">
+        <div className="hero__logo"></div>
+        <div className="hero__nav hero__nav--hidden left-0">
+          <nav className="main-nav inset-0 flex items-center">
+            <ul className="main-nav__list flex m-0">
+              {mainNavItems.map((navItem) => {
+                return (
+                  <li key={navItem.title} className="main-nav__item float-left mr-4 text-center">
+                    <a
+                      href={navItem.href}
+                      className="main-nav__link font-rex mr-3.75 text-base hover:text-orange-400 transition delay-150"
+                    >
+                      {navItem.title}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,12 +8,30 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'brand-blue': '#1DA1F2',
+        'brand-green': '#17BF63',
+        'header-bg': '#131313',
+      },
+      fontSize: {
+        xs: '.75rem',
+        sm: '.875rem',
+        tiny: '.875rem',
+        base: '1rem',
+        lg: '1.125rem',
+        xl: '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '4rem',
+        '7xl': '5rem',
+      },
+      fontFamily: {
+        rex: ['Rex-Bold', 'sans-serif'],
       },
     },
   },
+  variants: {},
   plugins: [],
-}
+};
