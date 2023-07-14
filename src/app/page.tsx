79f6f56a26953797c3e6db6a71ca5d72c76435bb
millana_chotubaeva <chotubaevam@gmail.com>
2023-07-14 07:01:56 -0700

    have done the component: added files, fixed styles w tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import PromoBanner from "./components/promo-banner/promo-banner";
+import { PromoBanner } from "./components/promo-banner/promo-banner";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-between p-24">
-      <h1>Barbershop</h1>
+    <main className="flex min-h-screen flex-col p-24 bg-slate-600">
+      <h1>Barbershop icon</h1>
       <PromoBanner />
     </main>
   );
