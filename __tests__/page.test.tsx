82f3040b8664d1438bc79ccab3d08c6d7e77581b
millana_chotubaeva <chotubaevam@gmail.com>
2023-07-17 10:06:49 -0700

    released CI/CD

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,5 +1,6 @@
 import { Header } from '../src/components/Header/Header';
 import { render, screen } from '@testing-library/react';
+import React from 'react';
 
 describe('Header', () => {
   it('should pass all the tests', () => {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import { Header } from '@/components/Header/Header';
+import { Header } from '../components/Header/Header';
+import React from 'react';
 
 export default function Home() {
   return (
