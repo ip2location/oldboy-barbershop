f80018df45cb86c50a44881fa6fa24f22277d516
Alinochka07 <alina011988@icloud.com>
2023-08-11 16:07:12 +0600

    fix pretier

--- src/Features/header/header.test.tsx
+++ src/Features/header/header.test.tsx
@@ -10,7 +10,7 @@ jest.mock('next/image', () => ({
 describe('HeaderMainPage', () => {
   test('pass the header main page buttons', async () => {
     render(<HeaderPage />);
-    
+
     const buttonRecord = screen.getByRole('button', { name: 'Запись онлайн' });
     await userEvent.click(buttonRecord);
 
