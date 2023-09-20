1975f11948ec2c622e9cb0903d96e47b1ef927f0
millana_chotubaeva <chotubaevam@gmail.com>
2023-09-19 20:53:27 -0700

    change old files and git to pull new files

--- src/Features/header/components/CityLanguageSelect/City/CitySelect.tsx
+++ src/Features/header/components/CityLanguageSelect/City/CitySelect.tsx
@@ -1,25 +1,10 @@
 import Link from 'next/link';
 import React, { ReactElement, useState } from 'react';
-import { useSetSelectedEntity } from '../../../../../hooks/useSetSelectedEntity';
-import { Cookies } from '../../../../../constants';
 import { LocationSelector } from '../../../../../components/LocationSelector/LocationSelector';
 import { Modal } from '../../../../../components/Modal/Modal';
 
-interface CitySelectProps {
-  cityValue?: string;
-}
-
-export const CitySelect = ({ cityValue = 'Бишкек' }: CitySelectProps): ReactElement => {
+export const CitySelect = (): ReactElement => {
   const [showModal, setShowModal] = useState(false);
-  const [cityInput, setCityInput] = useState('');
-
-  const { selectedEntity: selectedCity, setSelectedEntity: setSelectedCity } = useSetSelectedEntity(
-    cityValue,
-    Cookies.City,
-  );
-
-  const { selectedEntity: selectedCountry, setSelectedEntity: setSelectedCountry } =
-    useSetSelectedEntity(cityValue, Cookies.Country);
 
   const handleOpen = (event: React.MouseEvent<HTMLElement>) => {
     event.preventDefault();
@@ -27,37 +12,7 @@ export const CitySelect = ({ cityValue = 'Бишкек' }: CitySelectProps): Rea
     return showModal;
   };
 
-  const selectCity = (value: string) => {
-    setCookie(Cookies.City, value, { expires: SEVEN_DAYS });
-    setSelectedCity(value);
-  };
-
-  const selectCountry = (value: string) => {
-    if (value === 'Москва') {
-      const countryCode = 'RU';
-      setCookie(Cookies.Country, countryCode, { expires: SEVEN_DAYS });
-      setSelectedCountry(countryCode);
-    }
-    if (value === 'Бишкек') {
-      const countryCode = 'KG';
-      setCookie(Cookies.Country, countryCode, { expires: SEVEN_DAYS });
-      setSelectedCountry(countryCode);
-    }
-  };
-
-  const handlePickCity = (value: string) => {
-    selectCity(value);
-    selectCountry(value);
-    setShowModal(false);
-  };
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    event.preventDefault();
-    setCityInput(event.currentTarget.value);
-    return cityInput;
-  };
-
-return (
+  return (
     <>
       <Link
         href="/"
@@ -78,9 +33,6 @@ return (
         >
           Выбери свой <br /> барбершоп:
           <span className="m-0 text-orange-title font-rex text-[8.5px]">&#x25BC;</span>
-        </p>
-        <p className="city-select__city font-rex m-0 text-base">
-          {selectedCountry} - {selectedCity}
         </p>
       </Link>
       {showModal ? (
